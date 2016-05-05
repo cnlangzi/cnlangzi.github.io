a9129c038aa723ad60166ffca2b83bbdb5e2a923
Dayi Chen <imlangzi@qq.com>
2016-05-06 00:06:51 +0800

    ss

---
+++ files/ss/ss.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+arch=`uname -m`
+
+if [ $arch = "x86_64" ]
+then
+	wget http://cnlangzi.github.io/files/ss/x64/shadowsocks-server
+else
+	wget http://cnlangzi.github.io/files/ss/x32/shadowsocks-server
+fi
+
+
+service shadowsocks-server stop
+\cp -rf shadowsocks-server /usr/local/shadowsocks-server -f
+chmod +x /usr/local/shadowsocks-server
+
+chkconfig shadowsocks-server on
+service shadowsocks-server start
